ab770b94fd2b13134f6dba88ea948179ed162dcc
Vitaly Tolstikov <vitalytva@yandex.ru>
2013-02-27 22:49:54 +0400

    Initial

---
+++ WinMVVM/DataContextProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinMVVM {
+    [
+    ProvideProperty("DataContext",typeof(Control)),
+    ]
+    public class DataContextProvider : Component, IExtenderProvider {
+        bool IExtenderProvider.CanExtend(object extendee) {
+            return true;
+        }
+        public string GetDataContext(Control control) {
+            return "Test";
+        }
+        public void SetDataContext(Control control, string value) { 
+        }
+    }
+}
