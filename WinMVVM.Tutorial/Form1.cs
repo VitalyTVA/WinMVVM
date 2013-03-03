ce47c60f5bff8da9ad2594eb8b46b97e2885a3bb
Vitaly Tolstikov <vitalytva@yandex.ru>
2013-03-03 16:10:14 +0400

    Button with binding in tutorial

--- WinMVVM.Tutorial/Form1.cs
+++ WinMVVM.Tutorial/Form1.cs
@@ -7,11 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinMVVM;
 
 namespace WinMVVM.Tutorial {
     public partial class Form1 : Form {
+        MainViewModel viewModel = new MainViewModel();
         public Form1() {
             InitializeComponent();
+
+            this.SetDataContext(viewModel);
+            button1.SetBinding(() => button1.Text, new Binding("MessageText"));
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            viewModel.MessageText += "+";
         }
     }
 }
---
+++ WinMVVM.Tutorial/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinMVVM.Tutorial {
+    public class MainViewModel : ViewModelBase {
+
+        string messageText;
+
+        public string MessageText {
+            get { return messageText; }
+            set { Set(() => MessageText, ref messageText, value); }
+        }
+        
+    }
+}
