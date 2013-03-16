041edb39d25049c41e71dc23ce6b7585543411e6
Vitaly Tolstikov <vitalytva@yandex.ru>
2013-03-16 19:15:58 +0400

    ItemsSourceProvider

---
+++ WinMVVM/ItemsSourceProvider.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using WinMVVM.Utils;
+
+namespace WinMVVM {
+    public static class ItemsSourceProvider {
+        public static readonly AttachedProperty<IEnumerable> ItemsSourceProperty = AttachedProperty<IEnumerable>.Register(() => ItemsSourceProperty, new PropertyMetadata<IEnumerable>(null, OnItemsSourceChanged));
+        public static IEnumerable GetItemsSource(this Control control) {
+            return control.GetValue(ItemsSourceProperty);
+        }
+        public static void SetItemsSource(this Control control, IEnumerable value) {
+            control.SetValue(ItemsSourceProperty, value);
+        }
+        static void OnItemsSourceChanged(Control sender, AttachedPropertyChangedEventArgs<IEnumerable> e) {
+            
+        }
+    }
+}
---
+++ WinMVVM/Tests/ItemsSourceProviderTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace WinMVVM.Tests {
+    [TestFixture]
+    public class ItemsSourceProviderTests {
+        [Test]
+        public void BindToSimpleList() {
+
+        }
+    }
+}
