0bed05b7b3d5221db875648dbc0e7cd3d8f54fb1
Vitaly Tolstikov <vitalytva@yandex.ru>
2013-03-28 00:20:40 +0400

    DataGridViewItemsSourceTests

--- WinMVVM/Tests/ItemsSourceProvider/DataGridViewItemsSourceTests.cs
+++ WinMVVM/Tests/ItemsSourceProvider/DataGridViewItemsSourceTests.cs
@@ -10,36 +10,37 @@ using System.Windows.Input;
 using WinMVVM.Utils;
 
 namespace WinMVVM.Tests.ItemsSource {
-    //[TestFixture]
-    //public class DataGridViewItemsSourceTests : ItemsSourceTestsBase<DataGridView> {
-    //    protected override IList GetItems(DataGridView control) {
-    //        return GetItemsCore(control).ToList();
-    //    }
-    //    IEnumerable<object> GetItemsCore(DataGridView control) {
-    //        for(int i = 0; i < control.RowCount; i++) {
-    //            yield return control.Rows[i].DataBoundItem;
-    //        }
-    //    }
+    [TestFixture]
+    public class DataGridViewItemsSourceTests : ItemsSourceTestsBase<DataGridView> {
+        protected override IList GetItems(DataGridView control) {
+            return GetItemsCore(control).ToList();
+        }
+        IEnumerable<object> GetItemsCore(DataGridView control) {
+            for(int i = 0; i < control.RowCount; i++) {
+                yield return control.Rows[i].DataBoundItem;
+            }
+        }
 
-    //    protected override object GetDataSource(DataGridView control) {
-    //        return control.DataSource;
-    //    }
+        protected override object GetDataSource(DataGridView control) {
+            return control.DataSource;
+        }
 
-    //    protected override string[] GetItemsSourceDependentProperties() {
-    //        return new string[] { GetPropertyName(() => new DataGridView().DataSource) };
-    //    }
+        protected override string[] GetItemsSourceDependentProperties() {
+            return new string[] { GetPropertyName(() => new DataGridView().DataSource) };
+        }
 
-    //    protected override int GetSelectedIndex(DataGridView control) {
-    //        return control.Rows.Cast<DataGridViewRow>().FirstOrDefault(x => x.Selected).Return(x => x.Index, () => 1);
-    //    }
+        protected override int GetSelectedIndex(DataGridView control) {
+            return control.Rows.Cast<DataGridViewRow>().FirstOrDefault(x => x.Selected).Return(x => x.Index, () => -1);
+        }
 
-    //    protected override void SetSelectedIndex(DataGridView control, int value) {
-    //        control.Rows[value].Selected = true;
-    //    }
+        protected override void SetSelectedIndex(DataGridView control, int value) {
+            control.Rows[value].Selected = true;
+        }
 
-    //    protected override DataGridView CreateControl() {
-    //        var control = base.CreateControl();
-    //        return control;
-    //    }
-    //}
+        protected override DataGridView CreateControl() {
+            var control = base.CreateControl();
+            control.AllowUserToAddRows = false;
+            return control;
+        }
+    }
 }
